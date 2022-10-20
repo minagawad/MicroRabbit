6bf147af57ff39767b678c22fc6ca3001c24fb70
mina gerges <mgerges@getgroup.com>
2022-10-20 10:35:00 +0200

    add dependency container

---
+++ MicroRabbit/MicroRabbit.Infra.IOC/DependencyContainer.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Info.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MicroRabbit.Infra.IOC
+{
+    public class DependencyContainer
+    {
+       
+        public static void Register(IServiceCollection services)
+        {
+
+            services.AddTransient<IEventBus, RabbitMqBus>();
+
+        }
+    }
+}
